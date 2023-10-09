0fc4d215ce1954df09b79d1fbfa4c260d6742f20
Sourav <souravbadhan909@gmail.com>
2023-10-09 13:44:17 -0400

    added some changes to the file to make it work correctly.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,16 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
+  before_action :authenticate
+  
   def show
   end
+
+private
+
+def authenticate
+  authenticate_or_request_with_http_basic do |username, password|
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  end
+end
+
 end
